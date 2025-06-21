9f433c1bae5c83f5b7632c5c0d4544d24d96519f
Zelelo622 <egorcom27@gmail.com>
2025-06-21 19:16:26 +0300

    start settings proj

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [
+      tsconfigPaths(),
+      react({
+        babel: {
+          plugins: [["babel-plugin-styled-components", { displayName: true }]]
+        }
+      }),
+      svgr({ include: "**/*.svg" })
+    ],
+    server: {
+      port: parseInt(process.env.VITE_FRONT_PORT_DEV) ?? 8080,
+      strictPort: true
+    },
+    css: {
+      modules: {
+        scopeBehaviour: "local",
+        localsConvention: "camelCase"
+      }
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          minifyInternalExports: false,
+          manualChunks: {
+            react: ["react", "react-dom/client", "react-router-dom"],
+            mobx: ["mobx", "mobx-react", "mobx-react-lite", "mobx-utils"],
+            "styled-components": ["styled-components"]
+          }
+        }
+      },
+      chunkSizeWarningLimit: 300
+    }
+  });
+};
